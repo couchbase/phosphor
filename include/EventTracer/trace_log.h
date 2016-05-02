e438490735dff0f38739b3cc5b9adfe17a11aca5
Will Gardner <will.gardner@couchbase.com>
2016-05-02 18:24:06 +0100

    Fix spacing

--- include/EventTracer/trace_log.h
+++ include/EventTracer/trace_log.h
@@ -15,6 +15,7 @@
  *   limitations under the License.
  */
 
+
 class TraceLog {
 public:
     TraceLog& getInstance();
--- src/trace_buffer.cc
+++ src/trace_buffer.cc
@@ -17,27 +17,92 @@
 
 #include <stdexcept>
 
+#include "polyfill.h"
 #include "trace_buffer.h"
 
-TraceBufferChunk::TraceBufferChunk() {
-    next_free = chunk.begin();
+using chunk_ptr = std::unique_ptr<TraceBufferChunk>;
+
+TraceBufferChunk::TraceBufferChunk(size_t seq_no_, size_t buffer_index_)
+    : thread_id(std::this_thread::get_id()),
+      seq_no(seq_no_),
+      buffer_index(buffer_index_),
+      next_free(0) {
 }
 
 bool TraceBufferChunk::isFull() const {
-    return next_free == chunk.end();
+    return next_free == chunk.max_size();
 }
+
 
 size_t TraceBufferChunk::count() const {
-    return next_free - chunk.begin();
+    return next_free;
 }
 
-TraceEvent& TraceBufferChunk::addEvent() {
+size_t TraceBufferChunk::getIndex() const {
+    return buffer_index;
+}
+
+size_t TraceBufferChunk::getSeqno() const {
+    return seq_no;
+}
+
+
+void TraceBufferChunk::addEvent(TraceEvent&& event) {
     if(isFull()) {
         throw std::out_of_range("All events in chunk have been used");
     }
-    return *next_free++;
+    chunk[next_free++] = event;
 }
 
 const TraceEvent& TraceBufferChunk::operator[] (const int index) const {
     return chunk[index];
 }
+
+/**
+ * TraceBuffer implementation that stores events in a fixed-size
+ * vector of unique pointers to BufferChunks.
+ */
+class FixedTraceBuffer : public TraceBuffer {
+
+public:
+    FixedTraceBuffer(size_t seq_no_, size_t buffer_size_)
+        : seq_no(seq_no_),
+          buffer_size(buffer_size_) {
+        buffer.reserve(buffer_size);
+    }
+
+    ~FixedTraceBuffer() override {
+
+    }
+
+    std::unique_ptr<TraceBufferChunk> getChunk() override {
+        if(isFull()) {
+            throw std::out_of_range("The TraceBuffer is full");
+        }
+
+        auto index(buffer.size());
+        buffer.push_back(nullptr);
+        return make_unique<TraceBufferChunk>(seq_no, index);
+    }
+
+    void returnChunk(chunk_ptr&& chunk) override {
+        if(chunk->getSeqno() != seq_no) {
+            throw std::invalid_argument("Chunk is not from this buffer");
+        }
+
+        buffer.at(chunk->getIndex()) = std::move(chunk);
+    }
+
+    bool isFull() const override {
+        return buffer.size() >= buffer_size;
+    }
+
+    size_t getSequence() const override {
+        return seq_no;
+    }
+
+protected:
+    std::vector<chunk_ptr> buffer;
+    size_t seq_no;
+    size_t buffer_size;
+};
