72087b1c42eada8d1b5b95323467dae8f82a6f11
Will Gardner <will.gardner@couchbase.com>
2016-06-29 12:30:53 +0100

    Manually implement MockTraceEvent constructor
    MSVC2012 doesn't support `using` methods properly.

--- tests/module/trace_event_test.cc
+++ tests/module/trace_event_test.cc
@@ -130,8 +130,24 @@ TEST(TraceEvent, toJSONAlt) {
 
 class MockTraceEvent : public TraceEvent {
 public:
-    using TraceEvent::TraceEvent;
     using TraceEvent::typeToJSON;
+
+    MockTraceEvent(
+               const char* _category,
+               const char* _name,
+               Type _type,
+               size_t _id,
+               uint64_t _thread_id,
+               std::array<TraceArgument, phosphor::arg_count>&& _args,
+               std::array<TraceArgument::Type, phosphor::arg_count>&& _arg_types)
+        : TraceEvent(_category,
+                     _name,
+                     _type,
+                     _id,
+                     _thread_id,
+                     std::move(_args),
+                     std::move(_arg_types)) {
+    }
 };
 
 TEST(TraceEventTypeToJSON, Instant) {
