4504f8d9b01da041592a0d180194b5d682d34480
Will Gardner <will.gardner@couchbase.com>
2016-07-29 15:13:09 +0100

    Update doxygen to gh-pages script to work on jenkins
    Change-Id: I7df9eb098ae69a8937ca7f8e3f88d875d2a63745
    Reviewed-on: http://review.couchbase.org/66315
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Will Gardner <will.gardner@couchbase.com>

--- scripts/doxygen-to-gh-pages.sh
+++ scripts/doxygen-to-gh-pages.sh
@@ -4,6 +4,10 @@
 #   https://github.com/ReadyTalk/swt-bling/blob/master/.utility/push-javadoc-to-gh-pages.sh
 #   http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
+set -e
+
+CDIR=$(pwd)
+
 DOXYGEN_VER=doxygen-1.8.11
 DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
 DOXYGEN_URL="http://ftp.stack.nl/pub/users/dimitri/${DOXYGEN_TAR}"
@@ -16,24 +20,24 @@ doxygen_install()
 }
 
 doxygen_install
+pushd phosphor > /dev/null
 ${TMPDIR-/tmp}/${DOXYGEN_VER}/bin/doxygen
+popd phosphor > /dev/null
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+pushd $HOME > /dev/null
+git config --global user.email "github@couchbase.com"
+git config --global user.name "cb-robot"
+rm -rf gh-pages
+git clone --quiet --branch=gh-pages git@github.com:couchbase/phosphor.git gh-pages > /dev/null
 
-    echo -e "Publishing doxygen. \n"
+pushd gh-pages > /dev/null
+git rm -rf *
+cp -Rf $CDIR/phosphor/doxydocs/html/* .
+git add -f .
+git commit -m "Update documentation"
+git push -fq origin gh-pages
+popd > /dev/null
+rm -rf gh-pages
+popd > /dev/null
 
-    cd $HOME
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
-
-    cd gh-pages
-    git rm -rf *
-    cp -Rf $TRAVIS_BUILD_DIR/doxydocs/html/* .
-    git add -f .
-    git commit -m "[Travis] Update documentation"
-    git push -fq origin gh-pages
-
-    echo -e "Published doxygen to gh-pages. \n"
-
-fi
+echo -e "Published doxygen to gh-pages. \n"
