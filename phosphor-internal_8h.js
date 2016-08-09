de3063c390fbdd43871e2aec3a0c91dd0842add0
cb-robot <github@couchbase.com>
2016-08-09 08:59:40 -0700

    Update documentation

--- phosphor-internal_8h.js
+++ phosphor-internal_8h.js
@@ -1,5 +1,9 @@
 var phosphor_internal_8h =
 [
+    [ "PHOSPHOR_INTERNAL_CATEGORY_INFO", "phosphor-internal_8h.html#a8212ff223893d75c7d35053634ac259f", null ],
     [ "PHOSPHOR_INTERNAL_TRACE_EVENT", "phosphor-internal_8h.html#a09b88511f072ab140fe31c093460a949", null ],
-    [ "PHOSPHOR_INTERNAL_TRACE_EVENT0", "phosphor-internal_8h.html#adbef9ca962d58b38dd9c02cec2aa3ade", null ]
+    [ "PHOSPHOR_INTERNAL_TRACE_EVENT0", "phosphor-internal_8h.html#adbef9ca962d58b38dd9c02cec2aa3ade", null ],
+    [ "PHOSPHOR_INTERNAL_UID", "phosphor-internal_8h.html#acb80a95ade63584da667f5f3f286b8c5", null ],
+    [ "PHOSPHOR_INTERNAL_UID2", "phosphor-internal_8h.html#a1c5c33ff993cb04fb864c376243c2f80", null ],
+    [ "PHOSPHOR_INTERNAL_UID3", "phosphor-internal_8h.html#ac519874fbbcd0eb1c9c36c0770488dca", null ]
 ];
