bf600ae6270d71f71304f592d695532ba2358a39
cb-robot <github@couchbase.com>
2016-11-16 10:02:09 -0800

    Update documentation

--- classphosphor_1_1tools_1_1_file_stop_callback.js
+++ classphosphor_1_1tools_1_1_file_stop_callback.js
@@ -2,7 +2,6 @@ var classphosphor_1_1tools_1_1_file_stop_callback =
 [
     [ "FileStopCallback", "classphosphor_1_1tools_1_1_file_stop_callback.html#abab6d4389eeaf0ab061213877b81a043", null ],
     [ "~FileStopCallback", "classphosphor_1_1tools_1_1_file_stop_callback.html#af089efb2317f9be6a7bcbed046a4dcc2", null ],
-    [ "generateFilePath", "classphosphor_1_1tools_1_1_file_stop_callback.html#af0ef5ef9ab77857162c4446b4f66900f", null ],
-    [ "operator()", "classphosphor_1_1tools_1_1_file_stop_callback.html#afee88a1e38477822f0e68222dfaa1985", null ],
-    [ "file_path", "classphosphor_1_1tools_1_1_file_stop_callback.html#a2d47a138b3e5dc2e2c8c4f89d9cc6da2", null ]
+    [ "generateFilePathAsPtr", "classphosphor_1_1tools_1_1_file_stop_callback.html#a937d93628f34e9d94ea7adebf9a9ae9c", null ],
+    [ "operator()", "classphosphor_1_1tools_1_1_file_stop_callback.html#afee88a1e38477822f0e68222dfaa1985", null ]
 ];
