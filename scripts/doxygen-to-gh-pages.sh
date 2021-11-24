a74174a06083341617593f0a39db26a00964a54c
Blair Watt <18262146+udkyo@users.noreply.github.com>
2021-11-24 15:09:16 +0000

    BSL License updates - Couchbase Server Version Neo
    Change-Id: I3c8d5e179b2d65f3602a4392b394c5bf473b7be5
    Reviewed-on: https://review.couchbase.org/c/phosphor/+/166312
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- scripts/doxygen-to-gh-pages.sh
+++ scripts/doxygen-to-gh-pages.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Copyright 2016-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included in
+# the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+# file, in accordance with the Business Source License, use of this software
+# will be governed by the Apache License, Version 2.0, included in the file
+# licenses/APL2.txt.
+
 # Example taken from
 #   https://github.com/ReadyTalk/swt-bling/blob/master/.utility/push-javadoc-to-gh-pages.sh
 #   http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
